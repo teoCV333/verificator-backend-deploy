13ff706d9e85203be4e29f3a6cf87d9944b6e8fd
MCVdev <mateocarvajalvergara333@gmail.com>
2025-01-24 12:40:32 -0500

    test

--- server.js
+++ server.js
@@ -2,14 +2,9 @@ const express = require("express");
 const app = require("express")();
 const cors = require("cors");
 
-//const influencerRouters = require("./routes/influencerRoutes");
-
 app.use(cors())
 app.use(express.json());
 
-
-// Routes
-//app.use("/", influencerRouters);
 app.use("/", (req, res) => {
     res.send("Hello World");
 });
@@ -19,10 +14,6 @@ app.use((req, res) => {
     res.status(404).send("Sorry, can't find that!");
   });
 
-//const connectDB = require("./config/db");
-//const env = require("./config/env");
-
-//connectDB();
 
 app.listen(3000, () => {
     console.log(`the server is running on port: 3000`)
