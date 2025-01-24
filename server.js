4725ede79a8172cd98d101b6e9b58d4a01769cb2
MCVdev <mateocarvajalvergara333@gmail.com>
2025-01-24 12:41:50 -0500

    port updated

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.use((req, res) => {
   });
 
 
-app.listen(3000, () => {
-    console.log(`the server is running on port: 3000`)
+app.listen(8080, () => {
+    console.log(`the server is running on port: 8080`)
 })
