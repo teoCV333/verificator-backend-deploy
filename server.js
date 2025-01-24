f875fbe81681aeace1ffbbdd15b87f5a0133752e
MCVdev <mateocarvajalvergara333@gmail.com>
2025-01-24 12:43:55 -0500

    route updated

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const app = require("express")();
-const cors = require("cors");
+const app = express();
 
-app.use(cors())
 app.use(express.json());
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
