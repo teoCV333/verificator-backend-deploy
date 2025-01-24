861f0b9d3c03755cd918e35368771563f46fb4fa
MCVdev <mateocarvajalvergara333@gmail.com>
2025-01-24 12:13:34 -0500

    second commit

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
-const app = require("./app");
+const express = require("express");
+const app = require("express")();
+const cors = require("cors");
+
+//const influencerRouters = require("./routes/influencerRoutes");
+
+app.use(cors())
+app.use(express.json());
+
+
+// Routes
+//app.use("/", influencerRouters);
+app.use("/", (req, res) => {
+    res.send("Hello World");
+});
+
 //const connectDB = require("./config/db");
 //const env = require("./config/env");
 
